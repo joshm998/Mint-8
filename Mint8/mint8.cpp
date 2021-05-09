b5fa04149bf27672db75cbe74af32e15c11229a6
Joshua Mangiola <joshm982@gmail.com>
2021-05-09 21:42:42 +1000

    Adds Basic Functions
    Adds some basic code, loading from files, loading a font set into memory, c++ 17 standards and some new files (cpu.cpp, cpu.h, config.h)

---
+++ Mint8/mint8.cpp
@@ -0,0 +1,47 @@
+﻿// Mint8.cpp : Defines the entry point for the application.
+//
+
+#include "mint8.h"
+#include <fstream>
+
+using namespace std;
+using namespace config;
+
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
+
+mint8::mint8()
+{
+	//Setting to 0x200 for the first instruction
+	program_count_ = start_address;
+
+	//Loading the Font Sets into memory
+	for (unsigned int i = 0; i < font_set_size; i++)
+	{
+		mem_[font_set_start_address + i] = font_set[i];
+	}
+}
+
+void mint8::read_file(char const* file_name)
+{
+	ifstream rom_file;
+	rom_file.open(file_name);
+
+	if (rom_file.is_open())
+	{
+		for (int i = 0; i < max_rom_size; i++)
+		{
+			rom_file >> mem_[i];
+			cout << mem_[i] << " ";
+		}
+
+		rom_file.close();
+	}
+	else {
+		cerr << "File not found " << file_name << endl;
+	}
+}
---
+++ Mint8/mint8.h
@@ -0,0 +1,24 @@
+﻿// Mint8.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "config.h"
+
+class mint8
+{
+public:
+	mint8();
+	void read_file(char const* file_name);
+private:
+	uint8_t reg_[16]{};
+	uint8_t mem_[4096]{};
+	uint16_t index_{};
+	uint16_t program_count_{};
+	uint8_t delay_timer_{};
+	uint8_t sound_timer_{};
+	uint8_t buttons_[16]{};
+	uint32_t display_[64 * 32]{};
+	uint16_t op_code_;
+};
